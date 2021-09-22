29b34acb913b28b609aaabae561db7e3e08d7c48
TomBearpark <t.bearpark@googlemail.com>
2021-09-22 14:58:07 -0400

    Create ps1.R

---
+++ ps1.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+theme_set(theme_bw)
+
+
+# Question B
+# (i)
+a <- 30/ (168 + 30)
+print(a)
+
+# (ii)
+
+(0.152 -(0.63*0.06 + 0.1*0.85 )) /  .27
+
+
+# Question D
+sigma <- 5.670374419 * 10^{-8}
+T <- c(300, 283, 260)
+sigma * T^4
+
+G <- c(210, 130, 65)
+sigma * T^4 - G
+
+
+# Question F
