6f5118fd69b2107662d10eecdd8330ae74014efb
TomBearpark <t.bearpark@googlemail.com>
2021-11-09 09:56:18 -0500

    Update ps3.R

--- ps3.R
+++ ps3.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(purrr)
 library(ggplot2)
 library(glue)
+library(latex2exp)
 theme_set(theme_light())
 dir <- '/Users/tombearpark/Documents/princeton/2nd_year/term1/GEO_561/ps/ps3/'
 
@@ -19,3 +20,12 @@ map_dfr(range, k, 1,0,1) %>%
   ggplot() + 
   geom_line(aes(x = lambda, y = k, color = Configuration))
 ggsave(paste0(dir, "k.png"), height = 3, width = 7)
+
+
+ratio <- function(tau){
+  ((1 - exp(-tau / 2))*2) / (1 - exp(-tau ))
+}
+tibble(tau = seq(0, 5, 0.01), ratio = ratio(tau)) %>% 
+  ggplot() + 
+  geom_line(aes(x = tau, y = ratio))
+ggsave(paste0(dir, "ratio.png"), height = 4, width = 5)
